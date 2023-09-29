37907105207edf5bb731e234b076cf49917177a4
Mohamed Shabil <mohamedshabil231@gmail.com>
2023-09-29 11:23:44 +0530

    fix Bug - Adding product to wishlist without login

--- controller/wishlistController.js
+++ controller/wishlistController.js
@@ -3,10 +3,18 @@ const User = require('../model/userModel');
 
 
 exports.addToWishlist = catchAsync(async (req,res) => {
+    if(!req.user){
+        return res.status(200).json({
+            status:'false',
+            message:'Please Login add Product to whishlist',
+        })
+    }
     const product = req.body.productId
-    const user = await User.findOne({_id: req.user._id});
+    const user = await User.findById(req.user._id);
+    console.log(user)
     console.log(user.wishlist[0])
     const existingItem = user.wishlist.findIndex((item)=> item.product.equals(product));
+    console.log(existingItem);
     if(existingItem == -1){
         user.wishlist.push({product});
         await user.save();
@@ -19,7 +27,7 @@ exports.addToWishlist = catchAsync(async (req,res) => {
     }
     user.wishlist.splice(existingItem,1);
     await user.save();
-    res.status(200).json({
+    return res.status(200).json({
         status: 'success',
         wishlist : req.user.wishlist,
         operation:'removed',
@@ -30,7 +38,6 @@ exports.addToWishlist = catchAsync(async (req,res) => {
 exports.removeFromWishlist = catchAsync(async (req,res)=>{
     const product = req.body.productId
     console.log(product)
-    const user = await User.find({_id:req.user._id});
     const deleteItem  = await User.updateOne({_id:req.user._id},{$pull:{wishlist:{product:req.body.productId}}});
     console.log(deleteItem);
     req.flash('error','Item removed from the wishlist');
